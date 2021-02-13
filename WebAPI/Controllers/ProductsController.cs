0a4c028a91e0a2e12728fc19cc0bcbd49105d358
YusufAkkurt <ysfakkurt@hotmail.com>
2021-02-14 00:02:04 +0300

    WebAPI katmanı eklendi. Product nesnesi için Controller classı oluşturulup methodlar test edildi Startup.cs'de IoC Container'lar kullanılarak projemizdeki bağımlılıklar minimize edildi

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Loosely coupled = Gevşek bağımlılık
+        // Naming convention = İsimlendirme standartları
+        // IoC Container -- Inversion of Control = Değişimin kontrolü
+        readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            // Swagger
+            // Dependency chain = Bağımlılık zinciri
+            var result = _productService.GetAll();
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
