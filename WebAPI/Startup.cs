e4d9f6f2279e2b43e9a8dad8d337f70eb3283e0b
YusufAkkurt <ysfakkurt@hotmail.com>
2021-02-18 00:14:15 +0300

    Autofac ile AOP Kodlamaya geçiş yapıldı FluentValidation Eklendi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -34,8 +34,8 @@ namespace WebAPI
             services.AddControllers();
 
             // Ýçerisinde kullanýcýya özgü DATA TUTMUYORSAK singleton kullanmalýyýz.
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            // services.AddSingleton<IProductService, ProductManager>();
+            // services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
